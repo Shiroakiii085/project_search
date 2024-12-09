2d26ac50b0fcbaa4c78bde2dffbc632655af1521
Shiroakiii085 <abcxyztnt456789@gmail.com>
2024-12-09 15:50:47 +0700

    update_7

--- lungtung.js
+++ lungtung.js
@@ -1,22 +1,22 @@
 var app = "ID";
 app = window.prompt("Teleport Gate: Please choose ID!")
 console.log("ID: fb, tt, zl, ytb, codename:crack,ani , sc, lms, github,poke:rogue")
-if (app == "fb") {
+if (app == "fb"){
     alert ("Teleport Gate: Lét gâu to Phở Bò!")
     top.location = "https://www.facebook.com"
-} else if (app == "tt") {
+} else if (app == "tt"){
     alert ("Teleport Gate: Tóp Tóp is Coming!")
     top.location = "https://www.tiktok.com"
-} else if (app == "zl") {
+} else if (app == "zl"){
     alert ("Teleport Gate: Zép lào??!")
     top.location = "https://www.zalo.me"
-} else if (app == "ytb") {
+} else if (app == "ytb"){
     alert ("Teleport Gate: Diu túp for you!")
     top.location = "https://www.youtube.com"
-} else if (app == "codename:crack") {
+} else if (app == "codename:crack"){
     alert ("Teleport Gate: You choosen CODENAME:CRACK, Good Luck!")
     top.location = "https://hadoantv.com/"
-} else if (app == "sc") {
+} else if (app == "sc"){
     alert ("Teleport Gate: Let's the Music Time Begin!")
     top.location = "https://soundcloud.com"
 } else if (app == "terces"){
@@ -27,58 +27,66 @@ if (app == "fb") {
     if (secret == "rr"){
         alert ("Mysterious Voice: You enter the secret code, ID:R.R, Just For Fun!")
         top.location = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-    } else if (secret == "mixi") {
+    } else if (secret == "mixi"){
         console.log("MIXI ID: ytb, fb, dotoc, nimo, restream")
         alert ("Mysterious Voice: Hello MixiMoi Fan...!")
         var mixi = "address"
         mixi = window.prompt("Mysterious Voice: Choose the ID you want, type:mixi!")
-        if (mixi == "ytb:mixi") {
+        if (mixi == "ytb:mixi"){
             alert ("Mysterious Voice: Dâu! Hế lô xin chào cả nhà iu")
             top.location = "https://www.youtube.com/@MixiGaming3con"
-        } else if (mixi == "fb:mixi") {
+        } else if (mixi == "fb:mixi"){
             alert ("Mysterious Voice: You choose Facebook of MixiMoi, Good Luck!")
             top.location = "https://www.facebook.com/MixiGaming/"
-        } else if (mixi == "dotoc:mixi") {
+        } else if (mixi == "dotoc:mixi"){
             alert ("Mysterious Voice: Lướt Group tí đê!")
             top.location = "https://www.facebook.com/groups/mixigaming"
-        } else if (mixi == "nimo:mixi") {
+        } else if (mixi == "nimo:mixi"){
             alert ("Mysterious Voice: Chuyển sới đê cả lò nhà mình ơi, ai xem tiếp qua Nimotv/mixi nhá!")
             top.location = "https://www.nimo.tv/mixi"
-        } else if (mixi == "restream:mixi") {
+        } else if (mixi == "restream:mixi"){
             alert ("Mysterious Voice: Restream!")
             top.location = "https://mixigaming.com/"
         } else {
             alert ("Mysterious Voice: ID Invalid! Return to Teleport Gate")
-            top.location = " "
+            top.location = "https://shiroakiii085.github.io/project_search/"
         }
-    } else if (secret =="ani") {
+    } else if (secret =="ani"){
         console.log("Ani ID: ytb:ani, web:ani")
         alert ("Mysterious Voice: It's Quý Bửu Time!")
         var anime = "ani"
         anime = window.prompt("Mysterious Voice: Choose the ID you want, type:ani!")
-        if (anime == "ytb:ani") {
+        if (anime == "ytb:ani"){
             alert ("Mysterious Voice: Yes sir, go to Muse VN!")
             top.location = "https://www.youtube.com/c/MuseVi%E1%BB%87tNam"
-        } else if (anime == "web:ani") {
+        } else if (anime == "web:ani"){
             alert ("Mysterious Voice: Oke bro, go to AnimeHay.tv")
             top.location = "https://animehay.tv/"
         } else {
             alert ("Mysterious Voice: ID Invalid! Return to Teleport Gate")
-            top.location = " "
+            top.location = "https://shiroakiii085.github.io/project_search/"
         }
     } else if (secret =="admin:repo"){
-        var pass = "admin"
-        pass = window.prompt("Enter the ADMINISTRATOR_PASSWORD: ")
-        if(pass == "08052006"){
-            alert ("Mysterious Voice: Welcome back MASTER!!!")
-            top.location = "https://github.com/Shiroakiii085/project_search"
+        var pass1 = "admin"
+        pass1 = window.prompt("Mysterious Voice: Enter the ADMINISTRATOR_PASSWORD1: ")
+        if(pass1 == "08052006"){
+            alert("Mysterious Voice: Correct!!! Enter the ADMINISTRATOR PASSWORD2: ")
+            var pas2 = "whotao"
+            pass2 = window.prompt("Mysterious Voice: Enter the ADMINISTRATOR_PASSWORD2: ")
+            if(pass2 == "1507"){
+                alert("WELCOMBACK MASTER!!!!")
+                top.location = "https://github.com/Shiroakiii085/project_search"
+            } else {
+                alert ("Mysterious Voice: FUCK OFFF!!!")
+                top.location = "https://shiroakiii085.github.io/project_search/"
+            }
         } else {
             alert ("Mysterious Voice: FUCK OFFF!!!")
-            top.location = " "
+            top.location = "https://shiroakiii085.github.io/project_search/"
         }
     } else {
         alert ("Teleport Gate: Secret ID Invalid! Return to Teleport Gate!")
-        top.location = " "
+        top.location = "https://shiroakiii085.github.io/project_search/"
     }
 } else if (app == "lms"){
     alert ("Teleport Gate: You choose LMS-DNU!!! Let's go!")
@@ -91,5 +99,5 @@ if (app == "fb") {
     top.location = "https://pokerogue.net/"
 } else {
     alert ("Teleport Gate: ID Invalid!!!")
-    top.location = " "
+    top.location = "https://shiroakiii085.github.io/project_search/"
 }
